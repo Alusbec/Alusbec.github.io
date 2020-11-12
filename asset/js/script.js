a639e783c7f6710c0233b395b4b92608438f0705
Alusbec <adrienlsbc@gmail.com>
2020-11-12 12:37:07 +0100

    responsiveOk

---
+++ asset/js/script.js
@@ -0,0 +1,35 @@
+$(function(){
+
+    $('.pc1').click(function(){
+        alert('Je serais... Un ours');
+    });
+
+    $('.pc2').click(function(){
+        alert('Je serais... Un palmier');
+    });
+
+    $('.pc3').click(function(){
+        alert('Je serais... Le 4 novembre 2008 (élection de Barack Obama)');
+    });
+
+    $('.pc4').click(function(){
+        alert('Je serais... Un homme élastique');
+    });
+
+    $('.pc5').click(function(){
+        alert('Je serais... Give it up de KC & the Sunshine Band (funck)');
+    });
+
+    $('.pc6').click(function(){
+        alert('Je serais...  La jonglerie');
+    });
+
+    $('.pc7').click(function(){
+        alert('Je serais... La persévérance');
+    });
+
+    $('.pc8').click(function(){
+        alert('Je serais... Têtu');
+    });
+
+}); 
