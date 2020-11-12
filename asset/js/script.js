ce21f9d8129bd7ab5421fcda643f3479c9801e37
Alusbec <adrienlsbc@gmail.com>
2020-11-12 19:03:18 +0100

    JqPortraitChinois

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,35 +1,72 @@
-$(function(){
+$(function () {
 
-    $('.pc1').click(function(){
-        alert('Je serais... Un ours');
+    $('.pc1').mouseover(function () { // cible la div avec id green lui indique (mouseover) 
+        $('.pc1').html('Si j\'étais un animal, je serais... un Ours'); // qu'au passage de la sourie le texte change de colo
     });
 
-    $('.pc2').click(function(){
-        alert('Je serais... Un palmier');
+    $('.pc1').mouseout(function () {
+        $('.pc1').html('Si j\'étais un animal, je serais...')
     });
 
-    $('.pc3').click(function(){
-        alert('Je serais... Le 4 novembre 2008 (élection de Barack Obama)');
+    $('.pc2').mouseover(function () { // cible la div avec id green lui indique (mouseover) 
+        $('.pc2').html('Si j\'étais une plante, je serais... un Palmier'); // qu'au passage de la sourie le texte change de colo
     });
 
-    $('.pc4').click(function(){
-        alert('Je serais... Un homme élastique');
+    $('.pc2').mouseout(function () {
+        $('.pc2').html('Si j\'étais une plante, je serais...')
     });
 
-    $('.pc5').click(function(){
-        alert('Je serais... Give it up de KC & the Sunshine Band (funck)');
+    $('.pc3').mouseover(function () { // cible la div avec id green lui indique (mouseover) 
+        $('.pc3').html('Si j\'étais un événement historique, je serais... Le 4 novenbre 2008 (élection de Barack Obama'); // qu'au passage de la sourie le texte change de colo
     });
 
-    $('.pc6').click(function(){
-        alert('Je serais...  La jonglerie');
+    $('.pc3').mouseout(function () {
+        $('.pc3').html('Si j\'étais un événement historique, je serais...')
     });
 
-    $('.pc7').click(function(){
-        alert('Je serais... La persévérance');
+    $('.pc4').mouseover(function () { // cible la div avec id green lui indique (mouseover) 
+        $('.pc4').html('Si j\'étais un super pouvoir, je serais...Un homme élastique'); // qu'au passage de la sourie le texte change de colo
     });
 
-    $('.pc8').click(function(){
-        alert('Je serais... Têtu');
+    $('.pc4').mouseout(function () {
+        $('.pc4').html('Si j\'étais un super pouvoir, je serais...')
     });
+
+    $('.pc5').mouseover(function () { // cible la div avec id green lui indique (mouseover) 
+        $('.pc5').html('Si j\'étais une chanson? je serais...Give it up de KC & the Sunshine Band'); // qu'au passage de la sourie le texte change de colo
+    });
+
+    $('.pc5').mouseout(function () {
+        $('.pc5').html('Si j\'étais une chanson? je serais...')
+    });
+
+    $('.pc6').mouseover(function () { // cible la div avec id green lui indique (mouseover) 
+        $('.pc6').html('Si j\'étais un art, je serais... La jonglerie'); // qu'au passage de la sourie le texte change de colo
+    });
+
+    $('.pc6').mouseout(function () {
+        $('.pc6').html('Si j\'étais un art, je serais...')
+    });
+
+    $('.pc7').mouseover(function () { // cible la div avec id green lui indique (mouseover) 
+        $('.pc7').html('Si j\'étais une qualité, je serais... La persévérance'); // qu'au passage de la sourie le texte change de colo
+    });
+
+    $('.pc7').mouseout(function () {
+        $('.pc7').html('Si j\'étais une qualité, je serais...')
+    });
+
+    $('.pc8').mouseover(function () { // cible la div avec id green lui indique (mouseover) 
+        $('.pc8').html('Si j\'étais une mauvaise habitude, je serais... Têtu'); // qu'au passage de la sourie le texte change de colo
+    });
+
+    $('.pc8').mouseout(function () {
+        $('.pc8').html('Si j\'étais une mauvaise habitude, je serais...')
+    });
+
+
+
+
+
 
 }); 
